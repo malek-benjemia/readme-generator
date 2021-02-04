0b03a2c5e18717aa9e6ccbdd3246a114f287ff57
MalekBenJemia <malekbenjemia@gmail.com>
2021-02-04 17:43:53 -0500

    correct the license name glitch

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const promptUser = () => {
             type: 'checkbox',
             name: 'license',
             message: questions[6],
-            choices: ['None', 'Apache Licence 2.0', 'GNU General Public Licence v3.0', 'MIT Licence', 'BSD 2-Clause Simplified License', 'BSD 3-Clause New or Revised License', 'Boost Software Licence 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public Licence 2.0', 'GNU Affero General Public Licence v3.0', 'GNU General Public Licence v2.0', 'GNU Lesser General Public License v2.1','Mozilla Public License 2.0','The Unlicense'],
+            choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause Simplified License', 'BSD 3-Clause New or Revised License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1','Mozilla Public License 2.0','The Unlicense'],
             validate: nameInput => {
                 if (nameInput.length==1) {
                   return true;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,16 +3,16 @@
 function renderLicenseBadge(license) {
   if (license) {
     if (license=='None') {return '';};
-    if (license=='Apache Licence 2.0') {return '<img src=https://img.shields.io/badge/License-Apache%202.0-blue.svg>';} ;
-    if (license=='GNU General Public Licence v3.0'){return '<img src=https://img.shields.io/badge/License-GPLv3-blue.svg>';};
-    if (license== 'MIT Licence') {return '<img src=https://img.shields.io/badge/License-MIT-yellow.svg>';};
+    if (license=='Apache License 2.0') {return '<img src=https://img.shields.io/badge/License-Apache%202.0-blue.svg>';} ;
+    if (license=='GNU General Public License v3.0'){return '<img src=https://img.shields.io/badge/License-GPLv3-blue.svg>';};
+    if (license== 'MIT License') {return '<img src=https://img.shields.io/badge/License-MIT-yellow.svg>';};
     if (license== 'BSD 2-Clause Simplified License'){return '<img src=https://img.shields.io/badge/License-BSD%202--Clause-orange.svg>';};
     if (license=='BSD 3-Clause New or Revised License') {return '<img src=https://img.shields.io/badge/License-BSD%203--Clause-blue.svg>';};
-    if (license=='Boost Software Licence 1.0') {return '<img src=https://img.shields.io/badge/License-Boost%201.0-lightblue.svg>';};
+    if (license=='Boost Software License 1.0') {return '<img src=https://img.shields.io/badge/License-Boost%201.0-lightblue.svg>';};
     if (license=='Creative Commons Zero v1.0 Universal') {return '<img src=https://licensebuttons.net/l/zero/1.0/80x15.png>';};
-    if (license=='Eclipse Public Licence 2.0') {return '<img src=https://img.shields.io/badge/License-EPL%201.0-red.svg>';};
-    if (license=='GNU Affero General Public Licence v3.0') {return '<img src=https://img.shields.io/badge/License-AGPL%20v3-blue.svg>';};
-    if (license=='GNU General Public Licence v2.0') {return '<img src=https://img.shields.io/badge/License-GPL%20v2-blue.svg>';};
+    if (license=='Eclipse Public License 2.0') {return '<img src=https://img.shields.io/badge/License-EPL%201.0-red.svg>';};
+    if (license=='GNU Affero General Public License v3.0') {return '<img src=https://img.shields.io/badge/License-AGPL%20v3-blue.svg>';};
+    if (license=='GNU General Public License v2.0') {return '<img src=https://img.shields.io/badge/License-GPL%20v2-blue.svg>';};
     if (license=='GNU Lesser General Public License v2.1') {return '<img src=https://img.shields.io/badge/License-LGPL%20v3-blue.svg>';};
     if (license=='Mozilla Public License 2.0') {return '<img src=https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg>';};
     if (license=='The Unlicense') {return '<img src=https://img.shields.io/badge/license-Unlicense-blue.svg>';}
@@ -28,16 +28,16 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   if (license) {
     if (license=='None') {return '';};
-    if (license=='Apache Licence 2.0') {return '<a href="https://opensource.org/licenses/Apache-2.0">';} ;
-    if (license=='GNU General Public Licence v3.0'){return '<a href="https://www.gnu.org/licenses/gpl-3.0">';};
-    if (license== 'MIT Licence') {return '<a href="https://opensource.org/licenses/MIT">';};
+    if (license=='Apache License 2.0') {return '<a href="https://opensource.org/licenses/Apache-2.0">';} ;
+    if (license=='GNU General Public License v3.0'){return '<a href="https://www.gnu.org/licenses/gpl-3.0">';};
+    if (license== 'MIT License') {return '<a href="https://opensource.org/licenses/MIT">';};
     if (license== 'BSD 2-Clause Simplified License'){return '<a href="https://opensource.org/licenses/BSD-2-Clause">';};
     if (license=='BSD 3-Clause New or Revised License') {return '<a href="https://opensource.org/licenses/BSD-3-Clause">';};
-    if (license=='Boost Software Licence 1.0') {return '<a href="https://www.boost.org/LICENSE_1_0.txt">';};
+    if (license=='Boost Software License 1.0') {return '<a href="https://www.boost.org/LICENSE_1_0.txt">';};
     if (license=='Creative Commons Zero v1.0 Universal') {return '<a href="http://creativecommons.org/publicdomain/zero/1.0/">';};
-    if (license=='Eclipse Public Licence 2.0') {return '<a href="https://opensource.org/licenses/EPL-1.0">';};
-    if (license=='GNU Affero General Public Licence v3.0') {return '<a href="https://www.gnu.org/licenses/agpl-3.0">';};
-    if (license=='GNU General Public Licence v2.0') {return '<a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html">';};
+    if (license=='Eclipse Public License 2.0') {return '<a href="https://opensource.org/licenses/EPL-1.0">';};
+    if (license=='GNU Affero General Public License v3.0') {return '<a href="https://www.gnu.org/licenses/agpl-3.0">';};
+    if (license=='GNU General Public License v2.0') {return '<a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html">';};
     if (license=='GNU Lesser General Public License v2.1') {return '<a href="https://www.gnu.org/licenses/lgpl-3.0">';};
     if (license=='Mozilla Public License 2.0') {return '<a href="https://opensource.org/licenses/MPL-2.0">';};
     if (license=='The Unlicense') {return '<a href="http://unlicense.org/">';}
